43827eec7dbebf76d30bc380e7e70d2ba1723666
Alejandro Armas <lxarmas@gmail.com>
2025-01-15 11:42:44 -0800

    category_questionMark

--- src/components/CategoryPage.js
+++ src/components/CategoryPage.js
@@ -1,62 +1,93 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { fetchBooksByCategory } from '../api';
+import { Button } from 'react-bootstrap';
 import './CategoryPage.css';
+import Nav from './Nav';
+import BookDescription from './BookDescription';
 
 const CategoryPage = () => {
   const { categoryName } = useParams();
-  const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState( [] );
+  const [selectedBookId, setSelectedBookId] = useState( null );
   const navigate = useNavigate();
 
-  useEffect(() => {
+  useEffect( () => {
     const fetchBooks = async () => {
       try {
-        const response = await fetchBooksByCategory(categoryName);
-        setBooks(response);
-      } catch (error) {
-        console.error('Error fetching books:', error);
+        const response = await fetchBooksByCategory( categoryName );
+        setBooks( response );
+      } catch ( error ) {
+        console.error( 'Error fetching books:', error );
       }
     };
 
-    if (categoryName) {
+    if ( categoryName ) {
       fetchBooks();
     }
-  }, [categoryName]);
+  }, [categoryName] );
+
+  const toggleDescription = ( bookId ) => {
+    setSelectedBookId( ( prevSelectedBookId ) =>
+      prevSelectedBookId === bookId ? null : bookId
+    );
+  };
 
   return (
-    <div className="category-one">
-      <h1>Books in "{categoryName}" Category</h1>
+    <div className="category-page">
+      <Nav />
+      <h1 className="category-title">Books in "{categoryName}" Category</h1>
+
       {books && books.length > 0 ? (
         <div className="book-list">
-          {books.map((book) => (
-            <div className="book-card-category" key={book.id}>
-              <h2 className="book-title">{book.title}</h2>
-              <p className="book-author">by {book.author}</p>
-              <img
-                srcSet={`
-                  ${book.image_link}-small.jpg 500w,
-                  ${book.image_link}-medium.jpg 1000w,
-                  ${book.image_link}-large.jpg 1500w,
-                  ${book.image_link}-xlarge.jpg 3000w
-                `}
-                sizes="(max-width: 600px) 500px, (max-width: 1200px) 1000px, 1500px"
-                src={`${book.image_link}-medium.jpg`}
-                alt={book.title}
-                className="book-image"
-                onClick={() => {
-                  console.log(book.previewLink);
-                  if (book.previewLink) window.open(book.previewLink, '_blank');
-                }}
-                
-              />
-              <p className="book-description">{book.description}</p>
+          {books.map( ( book ) => (
+            <div className="book-card-category" key={book.book_id}>
+              <div className="book-card-header">
+                <Button
+                  className="question-button"
+                  onClick={() => toggleDescription( book.book_id )}
+                >
+                  ?
+                </Button>
+                <h2 className="book-title">{book.title}</h2>
+                <p className="book-author">by {book.author}</p>
+              </div>
+              <div className="book-card-content">
+                <img
+                  srcSet={`
+        ${book.image_link}-small.jpg 500w,
+        ${book.image_link}-medium.jpg 1000w,
+        ${book.image_link}-large.jpg 1500w,
+        ${book.image_link}-xlarge.jpg 3000w
+      `}
+                  sizes="(max-width: 600px) 500px, (max-width: 1200px) 1000px, 1500px"
+                  src={`${book.image_link}-medium.jpg`}
+                  alt={book.title}
+                  className="book-image"
+                  onClick={() => {
+                    if ( book.previewLink ) window.open( book.previewLink, '_blank' );
+                  }}
+                />
+
+              </div>
+              {selectedBookId === book.book_id && (
+                <div className="book-description-wrapper">
+                  <BookDescription
+                    description={book.description_book}
+                    onClick={() => toggleDescription( book.book_id )}
+                  />
+                </div>
+              )}
             </div>
-          ))}
+
+          ) )}
         </div>
       ) : (
         <p>No books found.</p>
       )}
-      <button className="back-button" onClick={() => navigate('/books')}>Back to Books</button>
+      <button className="back-button" onClick={() => navigate( '/books' )}>
+        Back to Books
+      </button>
     </div>
   );
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,15 @@ function Footer() {
             <div className="container text-center">
                 <div className="footer-content">
                     <div className="book-icon">
-                    <div className="book-icon">
-    <i className="fas fa-book fa-3x"></i>
-   
+                        <div className="book-icon">
+                            <i className="fas fa-book fa-3x"></i>
 
-</div>
+
+                        </div>
 
 
                     </div>
-                    <span style={{ color: 'white' }}>© 2024 WORTHY READS</span>
+                    <span style={{ color: 'black' }}>© 2024 WORTHY READS</span>
 
                 </div>
             </div>
