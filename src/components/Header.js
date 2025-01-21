29ff15cc7f290552f4a9c0844f6e3788e1911028
Alejandro Armas <lxarmas@gmail.com>
2025-01-21 15:48:43 -0800

    header_sytling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,22 @@
-
 import React from 'react';
 import './header.css';
 
-
 function Header() {
-    return (
-        <div className="header-content">
-
+  return (
+    <div className="header-content">
       <div className="header">
-        <h1>WORTHY READS</h1>
-        <p className="headerP">
-          Keep track of all the books you've read in one place!
-        </p>
-      </div>
 
+        <p className="headerTitle">
+          WORTHY READS
+        </p>
+        <img
+          src="/images/logo.png"
+          alt="Worthy Reads Logo"
+          className="header-logo"
+        />
       </div>
-    )
+    </div>
+  );
 }
+
 export default Header;
